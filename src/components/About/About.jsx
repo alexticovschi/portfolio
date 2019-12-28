41505dbcf00fd86266ffccc08862da1814a0ce5f
AlexTicovschi <alexmticovschi@gmail.com>
2019-12-28 19:49:53 +0000

    formatting text in About component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
       <div className="about__wrapper">
         <h1 className="about__title">About Me</h1>
         <p className="about__text">
-          From the moment I was able to finish my first project,I knew I was
+          From the moment I was able to finish my first project, I knew I was
           hooked into the world of web development. Web development has never
           been "just a passion" for me.It has offered an engaging challenge to
           continuously learn and improve my skills in creating better websites
@@ -16,8 +16,11 @@ const About = () => {
         <p>
           What started with just a curiosity of building something has become a
           full-fledged passion that only makes me more interested as the years
-          go by I've been interested in coding for a few years, finally taking
-          the decision in 2017 to invest my time to pursue this career path. To
+          go by.
+        </p>
+        <p>
+          I've been interested in coding for a few years, finally taking the
+          decision in 2017 to invest my time to pursue this career path. To
           achieve this goal I have devoted a large proportion of my time over
           the past 2 years to studying HTML, CSS, JavaScript, ReactJS, Node.js
           and MySQL. See my portfolio for examples of some web based apps I've
