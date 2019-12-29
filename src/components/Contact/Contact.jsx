d36090a0955ed250b7d153489c0f2c1bc52e64ff
AlexTicovschi <alexmticovschi@gmail.com>
2019-12-29 10:28:29 +0000

    adding button to Contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,30 +5,9 @@ const Contact = () => {
   return (
     <section className="contact" id="contact">
       <h1 className="contact__title">Get In Touch</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos beatae
-        repudiandae tenetur enim. Qui, aperiam temporibus! Repellat, ad natus ea
-        quis necessitatibus consectetur et, veniam consequuntur repudiandae rem
-        eaque doloremque repellendus veritatis ullam sint magni eveniet corrupti
-        ratione saepe fugit consequatur perferendis delectus cupiditate officia?
-        Nostrum laboriosam eum hic expedita odio ea neque, dolorum eligendi nam
-        praesentium numquam consequuntur officiis, mollitia sed et laudantium
-        quam at? Omnis odio ipsum esse recusandae fugiat vel optio, voluptatibus
-        saepe voluptas itaque voluptate quasi temporibus facilis iste
-        repellendus magnam harum, aspernatur ab dolores, doloremque dolore
-        inventore? Repudiandae, consequuntur distinctio perspiciatis voluptates
-        nostrum explicabo incidunt libero ipsum. Aperiam voluptas ipsa provident
-        nobis quod, rem quos vero sint obcaecati dicta doloribus minima ex in
-        eos minus deserunt recusandae at, ut nostrum unde. Nostrum eius, nisi ad
-        quos omnis blanditiis eligendi rem nesciunt vel praesentium voluptates
-        id aut itaque veritatis autem repellendus odit dolores, soluta atque.
-        Vel explicabo consequuntur earum porro blanditiis itaque pariatur, a
-        eaque eum veniam architecto mollitia totam quas nostrum beatae, aut
-        error excepturi modi alias obcaecati, reiciendis dignissimos dolores
-        facere. Nobis impedit ipsum commodi voluptatibus recusandae iure ducimus
-        quasi, excepturi assumenda quam rerum hic enim, laborum autem eligendi
-        harum repellendus aliquid voluptas nihil.
-      </p>
+      <button className="contact__btn">
+        <a href="mailto:alexmticovschi@gmail.com">Say Hello</a>
+      </button>
     </section>
   )
 }
