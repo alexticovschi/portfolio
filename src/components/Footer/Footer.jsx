ddb0541ce147c4cc34a3a1f676c037b1d73f927d
AlexTicovschi <alexmticovschi@gmail.com>
2019-12-31 13:04:42 +0000

    adding links to footer on smaller screens

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,51 @@
 import React from "react"
-
+import {
+  FaGithub,
+  FaLinkedin,
+  FaFreeCodeCamp,
+  FaCodepen,
+  FaInstagram,
+} from "react-icons/fa"
 import "./footer.scss"
 
 const Footer = () => {
   return (
     <footer className="footer">
+      <div className="footer__links">
+        <ul>
+          <li>
+            <a href="https://github.com/alexticovschi" target="blank">
+              <FaGithub className="icon" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/alex-ticovschi/"
+              target="blank"
+            >
+              <FaLinkedin className="icon" />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.freecodecamp.org/alexticovschi" target="blank">
+              <FaFreeCodeCamp className="icon" />
+            </a>
+          </li>
+          <li>
+            <a href="https://codepen.io/AlexTicovschi" target="blank">
+              <FaCodepen className="icon" />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/alexticovschi.art/"
+              target="blank"
+            >
+              <FaInstagram className="icon" />
+            </a>
+          </li>
+        </ul>
+      </div>
       <p className="footer__text">Built by Alex Ticovschi</p>
     </footer>
   )
