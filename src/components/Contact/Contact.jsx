45aa64243178bd3129abb5a2dc3974dc920532b8
Alex Ticovschi <alexmticovschi@gmail.com>
2020-09-09 18:55:09 +0100

    adding phone and fixing button padding

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,8 +5,14 @@ const Contact = () => {
   return (
     <section className="contact" id="contact">
       <h1 className="contact__title">Get In Touch</h1>
+
+      <div>
+        <a className="contact__phone" href="tel:07460 760 448">
+          07460 760 448
+        </a>
+      </div>
       <button className="contact__btn">
-        <a href="mailto:alexmticovschi@gmail.com">Say Hello</a>
+        <a href="mailto:alexmticovschi@gmail.com">Email Me</a>
       </button>
     </section>
   )
