6137fabf5abf8c0c2a4c653dc6f282d7edfbe2d1
Alex Ticovschi <alexmticovschi@gmail.com>
2020-12-12 07:28:10 +0000

    installing gatsby-plugin-react-helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
         path: `${__dirname}/src/images/`,
       },
     },
+    `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sass`,
